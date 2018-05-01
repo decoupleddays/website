34da21946c2383e437170a5b1f764dea84047e51
Mario Hernandez <mario.hernandez@mediacurrent.com>
2018-04-30 21:14:06 -0700

    Adds styled components and button component
    Added styled components gatsby plugin and created new component
    for button.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,23 +1,30 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Styled from 'styled-components'
+import Button from '../button/button'
 
-import './header.scss';
+import './header.scss'
 
 const Header = ({ siteTitle }) => (
   <div className="header">
     <div className="header__menu">
-      {/** Menu to go here when we have more pages **/}
-      <a href="" className="header__menu-register">Register Now</a>
-    </div>
+      {' '}
+      {/** Menu to go here when we have more pages **/}{' '}
+      <Button>Register now</Button>
+      <a href="" className="header__menu-register">
+        {' '}
+        Register Now{' '}
+      </a>{' '}
+    </div>{' '}
     <div className="header__hero">
       <h1>
         <Link to="/">
-          <span className="header__hero-site">Decoupled Drupal Days</span>
-          <span className="header__hero-location">New York City</span>
-          <span className="header__hero-date">Aug. 17-19, 2018</span>
-        </Link>
-      </h1>
-    </div>
+          <span className="header__hero-site"> Decoupled Drupal Days </span>{' '}
+          <span className="header__hero-location"> New York City </span>{' '}
+          <span className="header__hero-date"> Aug.17 - 19, 2018 </span>{' '}
+        </Link>{' '}
+      </h1>{' '}
+    </div>{' '}
   </div>
 )
 
