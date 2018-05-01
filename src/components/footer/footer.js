50b80b9fd638905a615f131a6704af6971cf6896
Grayson Hicks <graysonhicks@gmail.com>
2018-05-01 14:24:55 -0400

    footer as styled-components

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,10 +6,10 @@ import './footer.scss'
 const Footer = ({ siteTitle }) => (
   <FooterContainer>
     <Masthead>
-      <p className="footer__masthead--main">Decoupled Drupal Days</p>
-      <p>Aug. 17-19, 2018</p>
-      <p>John Jay College of Criminal Justice</p>
-      <p>524 W 59th St, New York, NY 10019</p>
+      <Title>Decoupled Drupal Days</Title>
+      <SubTitle>Aug. 17-19, 2018</SubTitle>
+      <SubTitle>John Jay College of Criminal Justice</SubTitle>
+      <SubTitle>524 W 59th St, New York, NY 10019</SubTitle>
     </Masthead>
     <Map>
       <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3021.631989835315!2d-73.9892263!3d40.7701182!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xefb0711781c769e!2sJohn+Jay+College+of+Criminal+Justice!5e0!3m2!1sen!2sus!4v1525136243936" />
@@ -35,6 +35,9 @@ const Masthead = styled.div`
   align-items: center;
   justify-content: center;
 `
+const Title = styled.p``
+
+const SubTitle = styled.p``
 
 const Map = styled.div``
 
