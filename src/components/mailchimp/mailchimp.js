c1679dada6c6d95241f3a591e1eb2eacb525629e
Grayson Hicks <graysonhicks@gmail.com>
2018-05-03 21:56:16 -0400

    basic form styles and functionality

--- src/components/mailchimp/mailchimp.js
+++ src/components/mailchimp/mailchimp.js
@@ -2,28 +2,67 @@ import React, { Component } from 'react'
 
 import Modal from 'react-responsive-modal'
 import MailchimpSubscribe from 'react-mailchimp-subscribe'
+import Button from '../button/button'
+
+import './modal-styles.css'
 
 const url =
   '//mc.us16.list-manage.com/subscribe/post?u=8f86f6d23b5155add11d536e5&id=58abd481c4'
 
 // // use the render prop and your custom form
-// const CustomForm = () => (
-//   <MailchimpSubscribe
-//     url={url}
-//     render={({ subscribe, status, message }) => (
-//       <div>
-//         <MyForm onSubmitted={formData => subscribe(formData)} />
-//         {status === "sending" && <div style={{ color: "blue" }}>sending...</div>}
-//         {status === "error" && <div style={{ color: "red" }}>{message}</div>}
-//         {status === "success" && <div style={{ color: "green" }}>Subscribed !</div>}
-//       </div>
-//     )}
-//   />
-// )
+const CustomForm = ({ status, message, onValidated }) => {
+  let email
+  const submit = () => {
+    if (
+      email &&
+      email.value.indexOf('@') > -1 &&
+      onValidated({
+        EMAIL: email.value,
+      })
+    ) {
+      return true
+    } else {
+      return onValidated({
+        EMAIL: email.value,
+      })
+    }
+  }
+
+  return (
+    <div
+      style={{
+        borderRadius: 2,
+        padding: 10,
+        display: 'inline-block',
+      }}
+    >
+      {status === 'sending' && <div style={{ color: 'blue' }}>sending...</div>}
+      {status === 'error' && <div style={{ color: 'red' }}>{message}</div>}
+      {status === 'success' && <div style={{ color: 'green' }}>{message}</div>}
+      <input
+        style={{ fontSize: '2em', padding: 5 }}
+        ref={node => (email = node)}
+        type="email"
+        placeholder="Your email"
+      />
+      <br />
+      <Button onClick={submit}>Submit</Button>
+    </div>
+  )
+}
 
 const MailChimpSubscribeModal = props => (
-  <Modal {...props} center>
-    <MailchimpSubscribe url={url} />
+  <Modal {...props} classNames={{ modal: 'custom-modal' }} center>
+    <MailchimpSubscribe
+      url={url}
+      render={({ subscribe, status, message }) => (
+        <CustomForm
+          status={status}
+          message={message}
+          onValidated={formData => subscribe(formData)}
+        />
+      )}
+    />
   </Modal>
 )
 
