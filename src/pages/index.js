3d1244d5fb62be53181e91113d9612c7b298dcdc
Mark Casias <mark.casias@mediacurrent.com>
2018-05-20 16:46:20 -0600

    Updating headline copy and intro paragraph

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ const IndexPage = ({ data }) => {
 
   return (
     <div className="main">
+      <h2>Decoupled Drupal Days gathers technologists, marketers and content professionals who build and use Drupal as a Content Service -- for decoupled front ends, content APIs, IoT, and more.</h2>
       <div className="about">
         <p>In its second year after a successful debut in 2017, Decoupled Drupal Days (D3) is a conference for architects, developers, and businesspeople involved in implementing decoupled Drupal architectures. The 2018 edition is scheduled for August 17–19, 2017 in New York City.</p>
         <p>Decoupled Drupal is the use of Drupal as a content service for other non-Drupal applications, whether they are in native desktop or mobile, universal JavaScript, set-top boxes, IoT devices, conversational interfaces, or other technologies.</p>
