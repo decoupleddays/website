fb052d4a3c29bf8c4d6fd9b994d04b6a3770409c
Jeff Tomlinson <jeff@fourkitchens.com>
2018-06-20 19:27:18 -0600

    Diversity and inclusion page.

---
+++ src/pages/diversity-inclusion.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+import Sponsors from '../components/sponsors/sponsors'
+
+const DiversityInclusionPage = ({ data }) => {
+
+  return (
+    <div className="main">
+      <h1>Diversity and Inclusion</h1>
+      <p>Decoupled Drupal Days believes in an inclusive conference and speaker lineup. We particularly encourage members of underrepresented groups, such as women, people of color, LGBTQ, trans* and GNC folk, people living with disabilities, and others, to submit proposals.</p>
+      <p>Decoupled Drupal Days 2018 will be devoting at least 25% of all accepted sessions to speakers of underrepresented groups or first-time speakers.</p>
+    </div>
+  )
+}
+
+export default DiversityInclusionPage;
