d8946f66edaaebee1f63eb6f420d38e948d24881
Mark Casias <mark.casias@mediacurrent.com>
2018-07-08 20:57:54 -0600

    Adding session content

--- src/pages/sessions.js
+++ src/pages/sessions.js
@@ -1,18 +1,52 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
-import Section from '../components/section/section'
 
 const IndexPage = ({ data }) => {
   const sessions = data.allNodeSession.edges
-
   return (
-    <div className="main">
-      {sessions.map((session, i) => (
-        <div>{session.node.title}</div>
-      ))}
+    <div className="sessions">
+      {sessions.map((session, i) => {
+        console.log(session.node);
+        const speakers = session.node.relationships.field_speakers;
+        return (
+          <Session key={i}>
+            <SessionTitle>{session.node.title}</SessionTitle>
+            <SessionSpeakers>
+              {speakers.map((speaker, i) => {
+                return (
+                  <SessionSpeaker>{speaker.title}</SessionSpeaker>
+                )
+              })}
+            </SessionSpeakers>
+            <SessionBody
+              dangerouslySetInnerHTML={{ __html:session.node.body.processed}}
+            />
+          </Session>
+        )}
+      )}
     </div>
   )
 }
+
+const Session = styled.div`
+  padding-bottom: 5px;
+`
+const SessionTitle = styled.div`
+  font-size: 1.1em;
+`
+const SessionSpeakers = styled.div`
+  font-weight: 300;
+`
+const SessionSpeaker = styled.div`
+  padding-bottom: 5px;
+`
+const SessionBody = styled.div`
+  padding-bottom: 5px;
+  strong {
+    font-weight: 100 !important;
+  }
+`
 
 export default IndexPage
 
@@ -23,7 +57,7 @@ export const query = graphql`
         node {
           title
           body {
-            summary
+            processed
           }
           field_session_length
           field_time
