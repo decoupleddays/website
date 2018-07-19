4395554c831cc90c0b388a8676e22ba053d3937d
Mark Casias <mark.casias@mediacurrent.com>
2018-07-18 21:04:34 -0600

    Adding date / time / room to sessions.

--- src/pages/sessions.js
+++ src/pages/sessions.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import GatsbyLink from 'gatsby-link'
-import styled from 'styled-components';
-
+import styled from 'styled-components'
+import Link from '../components/link/link'
+import { notDeepEqual } from 'assert';
+import moment from 'moment'
 
 const IndexPage = ({ data }) => {
   const sessions = data.allNodeSession.edges
@@ -9,31 +11,43 @@ const IndexPage = ({ data }) => {
     <div className="sessions">
       {sessions.map((session, i) => {
         const node = session.node
-        const speakers = node.relationships.field_speakers;
+        const speakers = node.r.speaker;
+        let speakerText = '';
+        if (speakers.length) {
+          speakerText = speakers.map((speaker, j) => {
+            return (
+              <SessionSpeaker key={j}>{speaker.title}</SessionSpeaker>
+            )
+          })
+        }
+        const room = node.r.room.name
+        const time = moment(node.time).utcOffset(-10).format("HH:mma")
         return (
           <Session key={i}>
+            <div>{node.datetime} {time} Room: {room}</div>
             <SessionTitle>
-              <GatsbyLink to={node.path.alias}>
+              <SessionLink to={node.path.alias}>
                 {node.title}
-              </GatsbyLink>
+              </SessionLink>
             </SessionTitle>
             <SessionSpeakers>
-              {speakers.map((speaker, i) => {
-                return (
-                  <SessionSpeaker>{speaker.title}</SessionSpeaker>
-                )
-              })}
+              {speakerText}
             </SessionSpeakers>
             <SessionLength>{node.field_session_length} minutes</SessionLength>
-            <SessionBody
-              dangerouslySetInnerHTML={{ __html:node.body.processed}}
-            />
           </Session>
         )}
       )}
     </div>
   )
 }
+
+const SessionLink = styled(Link)`
+  text-decoration: none;
+  color: #0b5394;
+  &:visited {
+    color: #0b5394;
+  }
+`
 
 const Session = styled.div`
   padding-bottom: 5px;
@@ -59,20 +73,22 @@ export default IndexPage
 
 export const query = graphql`
   query SessionsPageQuery {
-    allNodeSession(sort: {fields:[title], order: ASC}) {
+    allNodeSession(sort: {fields:[field_time], order: ASC}) {
       edges {
         node {
           title
           path {
             alias
           }
-          body {
-            processed
-          }
           field_session_length
-          field_time
-          relationships {
-            field_speakers {
+          datetime:field_time(formatString: "dd")
+          day:field_time(formatString: "dd")
+          time:field_time
+          r:relationships {
+            room: field_room {
+              name
+            }
+            speaker:field_speakers {
               title
               relationships {
                 field_company {
