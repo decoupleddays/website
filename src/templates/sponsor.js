92e99456de98eb9bb2b5208549c0d12d00dceaed
Mark Casias <mark.casias@mediacurrent.com>
2018-07-24 13:19:07 -0600

    Updating individual sponsor page layout

--- src/templates/sponsor.js
+++ src/templates/sponsor.js
@@ -1,14 +1,37 @@
 import React from "react"
+import styled from 'styled-components'
+
+const SponsorContainer = styled.div`
+  display: flex
+  align-items: center
+
+`
+const SponsorLogo = styled.div`
+  margin-right: 30px
+  text-align: center
+  p {
+    font-size: 1.1rem
+  }
+`
 
 const SponsorTemplate = ({data}) => {
   const node = data.nodeSponsors
   return (
-    <div>
-      <h1>{node.title}</h1>
-      <div
-        dangerouslySetInnerHTML={{ __html:node.body.processed}}
-      />
-    </div>
+    <SponsorContainer>
+      <SponsorLogo>
+        <img src={node.r.logo.localFile.childImageSharp.sizes.src} alt={node.title} />
+        <p>is a proud {node.level} sponsor!</p>
+      </SponsorLogo>
+      <div className="sponsor-content">
+        <h1>{node.title}</h1>
+        <div
+          dangerouslySetInnerHTML={{ __html:node.body.processed}}
+        />
+        <div className="sponsor-link">
+          For more information go to <a href={node.link.uri}>{node.link.uri}</a>
+        </div>
+      </div>
+    </SponsorContainer>
   );
 }
 
@@ -20,6 +43,13 @@ export const query = graphql`
       title
       body {
         processed
+      }
+      link:field_sponsor_link {uri}
+      level:field_sponsor_level
+      r:relationships {
+        logo:field_sponsor_logo {
+          localFile{childImageSharp{sizes(maxWidth: 250){src}}}
+        }
       }
     }
   }
