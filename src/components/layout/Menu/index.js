8e54be3bcf3fa1543b2e65211a529b83c782ce0d
Mark Casias <mark.casias@mediacurrent.com>
2019-04-24 20:01:15 -0600

    adding toggle action to menu

--- src/components/layout/Menu/index.js
+++ src/components/layout/Menu/index.js
@@ -1,33 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react'
+import { FaTimes, FaBars } from 'react-icons/fa'
 
 import Link from '../../fields/Link'
 import './style.scss'
 
-const Menu = (props) => (
-  <>
-    <a href="javascript: void(0)" class="main-menu-toggle">
-      <span class="open">
-        <svg class="svg-bars-dims">
-          <use xlinkHref="sprite/sprite.svg#bars"></use>
-        </svg>
-        <span class="menu text">Menu</span>
-      </span>
-      <span class="close">
-        <svg class="svg-close-dims">
-          <use xlinkHref="sprite/sprite.svg#close"></use>
-        </svg>
-        <span class="menu text">Close</span>
-      </span>
-    </a>
-    <nav class="main-menu">
-      <ul class="menu">
-        {/* <li><Link class="active" href="#">Sessions</Link></li> */}
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/venue">Venue</Link></li>
-        <li><Link to="/volunteer">Volunteer</Link></li>
-      </ul>
-    </nav>
-  </>
-);
+const Menu = props => {
+  const [active, setActive] = useState('')
 
-export default Menu;
+  const clickHandle = e => {
+    e.preventDefault()
+    if (active === 'active') {
+      setActive('')
+    } else setActive('active')
+    console.log(active)
+  }
+  return (
+    <>
+      <a onClick={clickHandle} class={`main-menu-toggle ${active}`}>
+        <span class="open">
+          <FaBars />
+          <span class="menu text">Menu</span>
+        </span>
+        <span class="close">
+          <FaTimes />
+          <span class="menu text">Close</span>
+        </span>
+      </a>
+      <nav class={`main-menu ${active}`}>
+        <ul class="menu">
+          {/* <li><Link class="active" href="#">Sessions</Link></li> */}
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/venue">Venue</Link>
+          </li>
+          <li>
+            <Link to="/volunteer">Volunteer</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default Menu
