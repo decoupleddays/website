07c170a83a8e6861e9868f56febc2752fdbce6b0
Mark Casias <markie@kanopistudios.om>
2021-02-15 14:00:42 -0700

    header sponsors and other minor fixes

--- src/components/layout/HeaderSponsors/index.js
+++ src/components/layout/HeaderSponsors/index.js
@@ -1,26 +1,24 @@
 import React from 'react'
+import './style.scss';
 const HeaderSponsors = props => {
   return (
-    <div className="header--sponsor-wrap">
-      <div className="container">
-        <div className="header--sponsors centered">
-          <div className="h3">Diamond Sponsors</div>
-          <div className="sponsor--grid diamond">
-            {props.sponsors.map(({ node }, key) => {
-              const uri = node.body ? node.path.alias : node.link.uri
-              return (
-                <div className="sponsor--item" key={key}>
-                  <a href={uri}>
-                    <img
-                      src={node.r.logo.localFile.cis.fluid.src}
-                      alt="sponsor logo"
-                    />
-                  </a>
-                </div>
-              )
-            })}
-          </div>
-        </div>
+    <div className="header--sponsors">
+      <div className="h3 header--sponsors__title">Diamond Sponsors</div>
+      <div className="sponsor--grid diamond">
+        {props.sponsors.map((node, key) => {
+          const uri = node.body ? node.path.alias : node.link.uri
+          return (
+            <div className="sponsor--item" key={key}>
+              <a href={uri}>
+                <img
+                  src={node.r.logo.localFile.cis.fluid.src}
+                  alt={node.title}
+                  className="sponsor--logo"
+                />
+              </a>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
--- src/components/layout/Layout/index.js
+++ src/components/layout/Layout/index.js
@@ -4,18 +4,48 @@ import {Helmet} from 'react-helmet'
 
 import TopBar from '../TopBar'
 import Header from '../Header'
-//import HeaderSponsors from '../HeaderSponsors'
+import HeaderSponsors from '../HeaderSponsors'
 //import Sponsors from '../../entities/Sponsors'
 import Footer from '../Footer'
 import FooterCopywrite from '../FooterCopywrite'
 import './style.scss'
 const Layout = props => {
-  const {site} = useStaticQuery(
+  const {site, sponsors} = useStaticQuery(
     graphql`
       query {
         site {
           siteMetadata {
             title
+          }
+        }
+        sponsors:allNodeSponsors(filter: {field_sponsor_level: {eq: "diamond"}, relationships: {field_tags: {elemMatch: {name: {eq: "2021"}}}}}) {
+          nodes {
+            id
+            drupal_id
+            title
+            body {
+              processed
+            }
+            path {
+              alias
+            }
+            link:field_sponsor_link {
+              uri
+            }
+            r: relationships {
+              logo: field_sponsor_logo {
+                filename
+                localFile {
+                  cis:childImageSharp {
+                    # Specify a fluid image and fragment
+                    # The default maxWidth is 800 pixels
+                    fluid(maxWidth:200) {
+                      ...GatsbyImageSharpFluid
+                    }
+                  }
+                }
+              }
+            }
           }
         }
       }
@@ -40,9 +70,9 @@ const Layout = props => {
       </Helmet>
       <TopBar />
       <Header />
-      {/* data.headerSponsors.edges.length > 0 && (
-        <HeaderSponsors sponsors={data.headerSponsors.edges} />
-      ) */}
+      { sponsors.nodes.length > 0 && (
+        <HeaderSponsors sponsors={sponsors.nodes} />
+      ) }
       <section className="width-1/2">{props.children}</section>
       <Footer />
       <FooterCopywrite />
