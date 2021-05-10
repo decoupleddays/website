927e707bba2382018101f6ec680235eac95b0859
Mark Casias <markie@kanopistudios.om>
2021-05-10 16:06:42 -0600

    removing CFP

--- src/components/layout/TopBar/index.js
+++ src/components/layout/TopBar/index.js
@@ -3,7 +3,7 @@ import './style.scss'
 
 const TopBar = () => (
   <div className="top-bar">
-    <div className="top-bar--inner container">
+    <div className="container top-bar--inner">
       <div className="top-bar--date">
         <p>
           <strong>July 14th - 15th</strong>
@@ -11,12 +11,12 @@ const TopBar = () => (
         </p>
       </div>
       <div className="top-bar--register">
-        {<a
+        {/**<a
           href="https://docs.google.com/forms/d/e/1FAIpQLScxop0OcspNwZFuHzlktUNoQ5Kqdmwugv79FhMiNo4zZBvfAQ/viewform"
           className="button secondary"
         >
           Submit a Session
-        </a>}
+        </a>**/}
         {/** <a
           href="https://www.eventbrite.com/e/decoupled-days-2020-registration-88407670723"
           className="button info"
