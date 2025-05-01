b9d83d26020bc8c027471c8305a1061bee1ea3a2
Mark Casias <mark.casias@lullabot.com>
2025-05-01 15:42:47 -0600

    Initial commit

---
+++ web/themes/decoupleddays/js/decoupleddays.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * decoupleddays behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.decoupleddays = {
+    attach (context, settings) {
+
+      // console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
